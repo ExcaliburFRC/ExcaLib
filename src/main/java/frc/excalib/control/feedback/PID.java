f57e1f005885a3e9be32e2f2f8c9019e87b35a7e
yoav cohen <yoavcohen2007@gmail.com>
2025-05-18 23:30:13 +0300

    ADD: PID class

---
+++ src/main/java/frc/excalib/control/feedback/PID.java
@@ -0,0 +1,58 @@
+package frc.excalib.control.math;
+
+import frc.excalib.additional_utilities.periodics.PeriodicScheduler;
+import frc.excalib.additional_utilities.periodics.PeriodicTask;
+
+import java.util.function.DoubleSupplier;
+
+public class PID extends PeriodicTask {
+    private final DoubleSupplier measurementSupplier;
+    private final double kp, ki, kd;
+    private final double periodSeconds;
+
+    private double setpoint;
+    private double output = 0.0;
+    private double integral = 0.0;
+    private double lastError = 0.0;
+    private boolean firstRun = true;
+
+    public PID(DoubleSupplier measurementSupplier, double setpoint, double kp, double ki, double kd, PeriodicScheduler.PERIOD period) {
+        super(() -> {}, period);
+        this.measurementSupplier = measurementSupplier;
+        this.setpoint = setpoint;
+        this.kp = kp;
+        this.ki = ki;
+        this.kd = kd;
+        this.periodSeconds = period.milliseconds / 1000.0;
+
+        super.setTask(() -> {
+            double measurement = measurementSupplier.getAsDouble();
+            double error = setpoint - measurement;
+
+            integral += error * periodSeconds;
+            double derivative = firstRun ? 0.0 : (error - lastError) / periodSeconds;
+
+            output = kp * error + ki * integral + kd * derivative;
+            lastError = error;
+            firstRun = false;
+        });
+    }
+
+    public double getOutput() {
+        return output;
+    }
+
+    public void setSetpoint(double setpoint) {
+        this.setpoint = setpoint;
+    }
+
+    public double getSetpoint() {
+        return setpoint;
+    }
+
+    public void reset() {
+        integral = 0.0;
+        lastError = 0.0;
+        firstRun = true;
+    }
+}
