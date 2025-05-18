1a8b95255853881bdee1c49cf4d5308c217f69be
yoav cohen <yoavcohen2007@gmail.com>
2025-05-18 23:04:00 +0300

    ADD: EMA filter

---
+++ src/main/java/frc/excalib/control/math/filters/data_fusion/EMAFilter.java
@@ -0,0 +1,31 @@
+package frc.excalib.control.math.filters.data_fusion;
+
+import frc.excalib.additional_utilities.periodics.PeriodicScheduler;
+import frc.excalib.additional_utilities.periodics.PeriodicTask;
+
+import java.util.function.DoubleSupplier;
+
+public class EMAFilter extends PeriodicTask {
+    private final double alpha;
+    private double filteredValue;
+    private boolean initialized = false;
+
+    public EMAFilter(DoubleSupplier input, double alpha, PeriodicScheduler.PERIOD period) {
+        super(() -> {}, period);
+        this.alpha = alpha;
+
+        super.setTask(() -> {
+            double raw = input.getAsDouble();
+            if (!initialized) {
+                filteredValue = raw;
+                initialized = true;
+            } else {
+                filteredValue = alpha * raw + (1.0 - alpha) * filteredValue;
+            }
+        });
+    }
+
+    public double getValue() {
+        return filteredValue;
+    }
+}
