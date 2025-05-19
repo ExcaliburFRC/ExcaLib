1257ff0863f0da5ea8a4a315f55865f83e6b6884
Keller6738 <itaykeller12345@gmail.com>
2025-05-19 15:33:24 +0300

    add: SwerveModuleType docs and getters

--- src/main/java/frc/excalib/swerve/swerve_utils/SwerveModuleType.java
+++ src/main/java/frc/excalib/swerve/swerve_utils/SwerveModuleType.java
@@ -2,6 +2,9 @@ package frc.excalib.swerve.swerve_utils;
 
 import edu.wpi.first.math.util.Units;
 
+/**
+ * an enum class contains swerve types that helps to initialize the swerve
+ */
 public enum SwerveModuleType {
     MK4i_l3(Units.inchesToMeters(4) * Math.PI / 6.12,
             Units.inchesToMeters(4) * Math.PI / 6.12,
@@ -10,11 +13,28 @@ public enum SwerveModuleType {
 
     public final double kVelocityConversionFactor, kPositionConversionFactor, kSteeringVelocityConversionFactor;
 
+    /**
+     * @param velocityConversionFactor The velocity conversion factor of the module
+     * @param positionConversionFactor The position conversion factor of the module
+     * @param rotationVelocityConversionFactor The rotational velocity conversion factor of the module
+     */
     SwerveModuleType(double velocityConversionFactor,
                      double positionConversionFactor,
                      double rotationVelocityConversionFactor) {
         kVelocityConversionFactor = velocityConversionFactor;
         kPositionConversionFactor = positionConversionFactor;
         kSteeringVelocityConversionFactor = rotationVelocityConversionFactor;
+    }
+
+    public double getVelocityConversionFactor() {
+        return kVelocityConversionFactor;
+    }
+
+    public double getPositionConversionFactor() {
+        return kPositionConversionFactor;
+    }
+
+    public double getSteeringVelocityConversionFactor() {
+        return kSteeringVelocityConversionFactor;
     }
 }
