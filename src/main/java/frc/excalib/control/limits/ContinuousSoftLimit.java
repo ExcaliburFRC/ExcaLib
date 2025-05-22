33d2c7d7512731e6a2b516438dbf8023251d4e3e
Keller6738 <itaykeller12345@gmail.com>
2025-05-22 19:20:27 +0300

    add: a function to get the unoptimized setpoint

--- src/main/java/frc/excalib/control/limits/ContinuousSoftLimit.java
+++ src/main/java/frc/excalib/control/limits/ContinuousSoftLimit.java
@@ -1,7 +1,6 @@
 package frc.excalib.control.limits;
 
 import edu.wpi.first.math.Pair;
-import frc.excalib.control.math.MathUtils;
 
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
@@ -24,13 +23,13 @@ public class ContinuousSoftLimit extends SoftLimit {
     }
 
     /**
-     * A method to get the limited setpoint.
+     * A method to get the optimized setpoint.
      *
      * @param measurement    the current state of the system
      * @param wantedSetpoint the wanted state of the system
-     * @return the limited setpoint
+     * @return the optimized setpoint
      */
-    public double getSetpoint(double measurement, double wantedSetpoint) {
+    public double getOptimizedSetpoint(double measurement, double wantedSetpoint) {
         Optional<Pair<Double, Double>> setpoints = continuousLimit(wantedSetpoint, measurement);
         if (setpoints.isEmpty()) { // the measurement and the setpoint are equals
             return wantedSetpoint;
@@ -47,7 +46,36 @@ public class ContinuousSoftLimit extends SoftLimit {
             return upperSetpoint;
         }
         return Math.abs(measurement - upperSetpoint) < Math.abs(measurement - lowerSetpoint) ?
-                        upperSetpoint :
-                        lowerSetpoint;
+                upperSetpoint :
+                lowerSetpoint;
+    }
+
+    /**
+     * A method to get the unoptimized setpoint.
+     *
+     * @param measurement    the current state of the system
+     * @param wantedSetpoint the wanted state of the system
+     * @return the unoptimized setpoint
+     */
+    public double getUnoptimizedSetpoint(double measurement, double wantedSetpoint) {
+        Optional<Pair<Double, Double>> setpoints = continuousLimit(wantedSetpoint, measurement);
+        if (setpoints.isEmpty()) { // the measurement and the setpoint are equals
+            return wantedSetpoint;
+        }
+
+        double upperSetpoint, lowerSetpoint;
+        upperSetpoint = setpoints.get().getFirst();
+        lowerSetpoint = setpoints.get().getSecond();
+
+        // if one of the setpoints exceeds the limit, return the other
+        if (upperSetpoint > super.getMaxLimit()) {
+            return lowerSetpoint;
+        } else if (lowerSetpoint < super.getMinLimit()) {
+            return upperSetpoint;
+        }
+
+        return Math.abs(measurement - upperSetpoint) < Math.abs(measurement - lowerSetpoint) ?
+                lowerSetpoint :
+                upperSetpoint;
     }
 }
